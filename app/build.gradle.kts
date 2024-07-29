2da252ffa1349a60f493e5b70e4831969894a97d
Ezzat  Emad <127358857+ezzatemad@users.noreply.github.com>
2024-07-29 13:02:10 +0300

    + marketing app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -68,4 +71,52 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Navigation Component
+    val navVersion = "2.7.6"
+    implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
+    implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
+
+
+    val lifecycleVersion = "2.7.0"
+    val archVersion = "2.2.0"
+
+    // ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
+    // LiveData
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion")
+
+    // Kotlin Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
+
+    // Dependency Injection
+    implementation("com.google.dagger:hilt-android:2.50")
+    kapt("com.google.dagger:hilt-compiler:2.50")
+
+
+
+    //RXAndroid
+    implementation("io.reactivex.rxjava3:rxandroid:3.0.2")
+    implementation("io.reactivex.rxjava3:rxjava:3.1.5")
+
+
+    //Calender
+    implementation("com.kizitonwose.calendar:view:2.4.0")
+
+
+    //Glide
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+
+    val lottieVersion = "6.4.0"
+    implementation("com.airbnb.android:lottie:$lottieVersion")
+
+    implementation("com.github.chnouman:AwesomeDialog:1.0.5")
+
+    //AwesomeDialog
+    implementation("com.github.chnouman:AwesomeDialog:1.0.5")
+    implementation("com.github.balwinderSingh1989:androidBestLocationTracker:2.0.10")
+
+}
+kapt {
+    correctErrorTypes = true
 }
--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
+
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -41,4 +44,44 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+
+    // Dependency Injection
+    implementation("com.google.dagger:hilt-android:2.50")
+    kapt("com.google.dagger:hilt-compiler:2.50")
+
+    //Roombase
+    val room_version = "2.6.1"
+
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
+
+    // To use Kotlin annotation processing tool (kapt)
+    kapt("androidx.room:room-compiler:$room_version")
+
+    // optional - Kotlin Extensions and Coroutines support for Room
+    implementation("androidx.room:room-ktx:$room_version")
+
+    implementation ("com.google.android.gms:play-services-mlkit-text-recognition:19.0.0")
+    implementation("com.google.code.gson:gson:2.8.6")
+
+
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.jakewharton.retrofit:retrofit2-kotlin-coroutines-adapter:0.9.2")
+
+
+    //Work Manager
+    val work_version = "2.9.0"
+    implementation ("androidx.work:work-runtime-ktx:$work_version")
+
+    // Firebase Messegeing
+    implementation("com.google.firebase:firebase-messaging")
+
+}
+
+kapt {
+    correctErrorTypes = true
 }
