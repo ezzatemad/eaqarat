e56cd8ba336c345f2b57d9c269a2705009b2d390
Ezzat  Emad <127358857+ezzatemad@users.noreply.github.com>
2024-07-29 23:30:02 +0300

    make register and login with jetpack compose

---
+++ app/src/main/java/com/example/marketingapp/MyApp.kt
@@ -0,0 +1,4 @@
+package com.example.marketingapp
+
+class MyApp {
+}
---
+++ app/src/main/java/com/example/marketingapp/splash/SplashActivity.kt
@@ -0,0 +1,36 @@
+package com.example.marketingapp
+
+import android.os.Bundle
+import android.window.SplashScreen
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.marketingapp.ui.theme.splasch_background
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SplashActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SplashScreen()
+        }
+    }
+}
+
+@Composable
+fun SplashScreen(){
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(splasch_background)
+    )
+}
+
+
